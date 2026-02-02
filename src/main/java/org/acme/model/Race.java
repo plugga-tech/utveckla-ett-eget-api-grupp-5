8dae6e51647d2b2c3da9c8c1f106c1e018c067b1
Joel Seger <joelseger87@gmail.com>
2026-02-02 12:45:07 +0100

    adds id to entity, so docker and quarkus work properly

--- src/main/java/org/acme/model/Race.java
+++ src/main/java/org/acme/model/Race.java
@@ -1,10 +1,25 @@
 package org.acme.model;
 
+import java.util.UUID;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Race {
 
-    
+    @Id
+    private UUID uuid;
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid() {
+        UUID uuid = UUID.randomUUID();
+        this.uuid = uuid;
+    }
+
+      
 
 }
